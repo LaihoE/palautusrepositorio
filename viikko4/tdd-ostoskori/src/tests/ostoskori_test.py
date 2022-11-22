7047115c152ae6fa8051ca88b17e3a321232e483
Laiho <emillaiho@hotmail.fi>
2022-11-22 08:05:57 +0200

    alotetaan kunnolla alusta tdd tyyliin

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -8,17 +8,4 @@ class TestOstoskori(unittest.TestCase):
 
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
         self.assertEqual(self.kori.hinta(), 0)
-
-    
-    def test_lisaa_tuotteita_koriin(self):
-        t = Tuote("banaani", 69)
-        self.kori.lisaa_tuote(t)
-        self.assertAlmostEqual(self.kori.kaikki_ostokset["banaani"].lukumaara(), 1)
-
-    def test_poista_korista(self):
-        t = Tuote("banaani", 69)
-        self.kori.lisaa_tuote(t)
-        self.kori.poista_tuote(t)
-        self.assertAlmostEqual(len(self.kori.kaikki_ostokset), 0)
-
- 
+        self.assertEqual(self.kori.tavaroita_korissa(), 0)
