40f040c90fb2197268c9f8da2fd005e0e736c8ff
Laiho <emillaiho@hotmail.fi>
2022-11-22 08:33:01 +0200

    9. tuotteen lisaamisen jalkeen ostokset palauttaa oikean tuotteen

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -62,3 +62,11 @@ class TestOstoskori(unittest.TestCase):
 
         ostokset = self.kori.ostokset()
         self.assertEqual(len(ostokset), 1)
+    
+    def test_yhden_tuotteen_lisaamisen_jalkeen_ostokset_oikea_nimi_ja_n_yksi(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+
+        ostokset = self.kori.ostokset()
+        self.assertEqual(ostokset[0].tuote.nimi(), "Maito")
+        self.assertEqual(len(ostokset), 1)
