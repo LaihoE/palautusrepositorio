8eae78b298c488232b66a9a0f8fd2315c22a087f
Laiho <emillaiho@hotmail.fi>
2022-11-22 08:39:21 +0200

    12. sama kun 11?

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -89,3 +89,13 @@ class TestOstoskori(unittest.TestCase):
 
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
         self.assertEqual(len(self.kori.ostokset()), 1)
+    
+    def test_ostos_maara_pysyy_yksi_kun_kaksi_samaa_tuotetta_2(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
+        self.assertEqual(len(self.kori.ostokset()), 1)
