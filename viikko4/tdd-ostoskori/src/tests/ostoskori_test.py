baef180126e62eda03871bb41fa0ecdefb95feb7
Laiho <emillaiho@hotmail.fi>
2022-11-22 08:24:41 +0200

    6. kaksi samaa tuotetta tavaroita 2

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -37,3 +37,12 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
 
         self.assertEqual(self.kori.hinta(), 5)
+    
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_hinta_oikein(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
